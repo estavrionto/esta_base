40eae05971415727fae1a3cb4dbc1428ddbc2ce0
estavrionto <estavrionto@gmail.com>
2023-09-29 00:49:05 -0400

    added ipynb BAR/DOTA, configs for dellAW setup

--- configs/dellAW/README.py
+++ configs/dellAW/README.py
@@ -73,4 +73,123 @@
 # echo "Xft.dpi: 96" >> ~/.Xresources
 
 
-# %command% -vulkan -vulkan_disable_graphics_pipeline_library -vulkan_disable_steam_shader_cache
+# %command%  -vulkan_disable_graphics_pipeline_library -vulkan_disable_steam_shader_cache
+# -gl
+# +fps_max 60 -high  -map dota -novid -nojoy -novr -vulkan -vulkan_disable_graphics_pipeline_library -vulkan_disable_steam_shader_cache
+
+
+# git config
+# git config --global user.name "estavrionto"
+# git config --global user.email estavrionto@gmail.com
+
+# pretty ls with folder sizes
+# alias lll='python3 ~/esta_base/scripts/ls_dir_size.py'
+
+
+# sudo nala install xclip xdotool maim
+
+# installing lutris
+# https://software.opensuse.org/download.html?project=home%3Astrycore&package=lutris
+
+
+# echo 'deb http://download.opensuse.org/repositories/home:/strycore/Debian_Testing/ /' | sudo tee /etc/apt/sources.list.d/home:strycore.list
+# curl -fsSL https://download.opensuse.org/repositories/home:strycore/Debian_Testing/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_strycore.gpg > /dev/null
+# sudo apt update
+# sudo apt install lutris
+# sudo nala install fonts-noto
+
+
+# 071523
+# cd /etc/network
+# sudo cat interfaces
+# sudo cp ./interfaces ./interfaces.save
+# sudo systemctl enable systemd-networkd
+
+# started networkmanager sefvicesu
+
+
+# 083123
+# (base) ab@debian:~/Documents$ nvidia-smi
+# Thu Aug 31 07:04:10 2023       
+# +-----------------------------------------------------------------------------+
+# | NVIDIA-SMI 525.125.06   Driver Version: 525.125.06   CUDA Version: 12.0     |
+# |-------------------------------+----------------------+----------------------+
+# | GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |
+# | Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |
+# |                               |                      |               MIG M. |
+# |===============================+======================+======================|
+# |   0  NVIDIA GeForce ...  On   | 00000000:01:00.0  On |                  N/A |
+# | N/A   62C    P0    29W /  88W |    909MiB /  6144MiB |      1%      Default |
+# |                               |                      |                  N/A |
+# +-------------------------------+----------------------+----------------------+
+                                                                               
+# +-----------------------------------------------------------------------------+
+# | Processes:                                                                  |
+# |  GPU   GI   CI        PID   Type   Process name                  GPU Memory |
+# |        ID   ID                                                   Usage      |
+# |=============================================================================|
+# |    0   N/A  N/A      1170      G   /usr/lib/xorg/Xorg                504MiB |
+# |    0   N/A  N/A      1779      G   ...169271152617974388,262144      156MiB |
+# |    0   N/A  N/A      1840      G   ...961179774181519899,262144        7MiB |
+# |    0   N/A  N/A      2118      G   ...RendererForSitePerProcess       70MiB |
+# |    0   N/A  N/A     28363      G   ...veSuggestionsOnlyOnDemand      165MiB |
+# +-----------------------------------------------------------------------------+
+# nvidia 535 driver install
+# https://www.reddit.com/r/debian/comments/15didi9/comment/ju7pp7u/?utm_source=share&utm_medium=web2x&context=3
+# https://www.linuxcapable.com/install-nvidia-drivers-on-debian/
+
+# remove Previous Nvidia Installations
+# 	First, use a blanket command to remove any traces of Nvidia installation on your system.
+# 	sudo apt autoremove nvidia* --purge
+# 	To remove the runfile type of installation, use the following command.
+# 	sudo /usr/bin/nvidia-uninstall
+# 	Lastly, remove the CUDA toolkit runfile installation using the following command.
+# 	sudo /usr/local/cuda-X.Y/bin/cuda-uninstall
+# 2nd Method: Install Nvidia Drivers – Nvidia Repository
+# 	Determine your Graphics Card
+# 		The first step for users with aging NVIDIA Graphics cards is to determine what it is and if it is supported, users with brand new cards can skip this part as there is no doubt they will be supported.
+# 		First, find your graphics card module.
+# 		lspci | grep -e VGA
+# 			01:00.0 VGA compatible controller: NVIDIA Corporation GP106M [GeForce GTX 1060 Mobile] (rev a1)
+
+# 	First, ensure you install the following packages, which may be already installed.
+# 		sudo nala install dirmngr ca-certificates software-properties-common apt-transport-https dkms curl -y
+
+# 	Import GPG key for Debian 11 Bullseye (using debian11 as debian12 isnt available):
+# 		curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/3bf863cc.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nvidia-drivers.gpg > /dev/null 2>&1
+# 	Import Nvidia Repository for Debian 11 Bullseye:
+# 		echo 'deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/ /' | sudo tee /etc/apt/sources.list.d/nvidia-drivers.list
+# 	Enable the CONTRIB repository. Run this even if you already have it enabled to be safe.
+# 		sudo add-apt-repository contrib
+# 	Install Nvidia Drivers – Proprietary or Opensource options
+# 	Update your sources list to reflect the newly added repository.
+# 		sudo apt update
+# 	Next, install the latest NVIDIA drivers.
+# 	Install NVIDIA Drivers With Cuda Support (Proprietary):
+# 		sudo apt install nvidia-driver cuda nvidia-smi nvidia-settings
+# last
+# 	Reboot your system once done.
+# 		sudo reboot
+# 	Verify the installation by running NVIDIA-SMI as the manual installation steps showed beforehand.
+# 		nvidia-smi
+# Install 32-bit Support for Nvidia Drivers
+# 	32-bit support can be easily enabled and installed first. Install the 64-bit drivers above, then proceed with the following steps.
+# 	First, enable 32-bit architecture.
+# 	sudo dpkg --add-architecture i386
+# 	Update the APT-CACHE to reflect the changes to the architecture.
+# 	sudo apt update
+# 	Install 32-bit support, and remove the Cuda package “libcuda1-i386” for those users not requiring it.
+# 	sudo apt install libcuda1-i386 nvidia-driver-libs-i386
+# 	Reboot your PC.
+# 	sudo reboot
+
+
+used:
+	sudo apt autoremove nvidia* --purge
+	sudo /usr/bin/nvidia-uninstall
+	sudo /usr/local/cuda-X.Y/bin/cuda-uninstall
+	lspci | grep -e VGA
+	sudo nala install dirmngr ca-certificates software-properties-common apt-transport-https dkms curl -y
+	curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/3bf863cc.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nvidia-drivers.gpg > /dev/null 2>&1
+	echo 'deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/ /' | sudo tee /etc/apt/sources.list.d/nvidia-drivers.list
+	sudo add-apt-repository contrib
---
+++ scripts/ls_dir_size.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import subprocess
+
+def sh(Command):
+    return subprocess.check_output(Command,shell=True,universal_newlines=True)
+import sys
+import json
+
+# print('test')
+
+t = 'du -h -d 1 .|sort -h -r'
+t = [i.split('\t') for i in sh(t).split('\n')]
+t = [[j for j in i if j != ''] for i in t]
+t = [i for i in t if i !=[]]
+
+total = t[0][0]
+t = t[1:]
+s ={i[1][2:]: i[0] for i in t}
+# print()
+# for i in s:
+#     print(s[i],i)
+
+# print()
+
+t = 'ls -Alh --sort=size | grep -v "^d"'
+t = [i.split(' ') for i in sh(t).split('\n')]
+t = [[j for j in i if j != ''] for i in t]
+f = [i for i in t if len(i)>5]
+# for i in f:
+#     print(i)
+# print()
+
+t = 'ls -Alh | grep "^d"| sort -h'
+t = [i.split(' ') for i in sh(t).split('\n')]
+t = [[j for j in i if j != ''] for i in t]
+t = [i for i in t if len(i)>5]
+d = {i[-1]:i for i in t}
+# for i in d:
+#     print(d[i],i)
+# print()
+
+a = []
+for i in s:
+    ii = d[i]
+    ii = ii[:4]+[s[i]]+ii[5:]
+    a.append(ii)
+
+a = a+f
+
+print(f'total {total}')
+
+for i in a:
+    print('\t'.join(i))
